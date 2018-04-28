4d84dab094876ff1c4f6e8ef53ed84f9ba827fde
thanhtin <thanhtinqn0903039105@gmail.com>
2018-04-28 10:11:12 +0700

    +NaiveMaxflow implementation

--- MaxFlow/MaxFlow/MaxFlow/NaiveMaxflow.cpp
+++ MaxFlow/MaxFlow/MaxFlow/NaiveMaxflow.cpp
@@ -1,166 +1,166 @@
-//#include <iostream>
-//#include <queue>
-//using namespace std;
-//
-//int const MAX = 100;
-//int const maxC = 1000;
-//int c[MAX][MAX], f[MAX][MAX], cf[MAX][MAX];
-//int path[MAX]; // trace path
-//			   // n is the number of vertices
-//			   //A is source vertice
-//			   //B is sink vertice
-//int n, A, B;
-//
-////input the arguments
-//void Enter()
-//{
-//	// m is the number of edges
-//	int m, u, v;
-//	for (int i = 0; i < MAX; i++)
-//	{
-//		for (int j = 0; j < MAX; j++)
-//		{
-//			c[i][j] = 0;
-//		}
-//	}
-//	cin >> n >> m >> A >> B;
-//	A--; // the first vertice begins at 0
-//	B--; // the sink vertice ends at n - 1
-//	for (int i = 0; i< m; i++)
-//	{
-//		cin >> u >> v; // two adjacent vertices
-//		cin >> c[u - 1][v - 1]; // weight
-//	}
-//}
-//// find increasing flow
-//void CreateGf()
-//{
-//	//Build cf from c and f
-//	for (int u = 0; u < n; u++)
-//	{
-//		for (int v = 0; v <n; v++)
-//		{
-//			cf[u][v] = maxC;
-//		}
-//	}
-//	for (int u = 0; u < n; u++)
-//	{
-//		for (int v = 0; v <n; v++)
-//		{
-//			if (c[u][v] > 0) // if (u,v) is a bow in G
-//			{
-//				if (f[u][v] < c[u][v])
-//				{
-//					cf[u][v] = c[u][v] - f[u][v]; // normal bow
-//				}
-//				if (f[u][v] > 0)// inverse bow
-//				{
-//					cf[v][u] = -f[u][v];
-//				}
-//			}
-//		}
-//	}
-//}
-////find a path from A to B using BFS
-//bool FindPath()
-//{
-//	queue<int> q;
-//	bool isFound = false;
-//	bool visited[MAX] = { 0 };
-//	q.push(A);
-//	visited[A] = true;
-//	while (!q.empty())
-//	{
-//		int s = q.front();
-//		q.pop();
-//		for (int i = 0; i < n; i++)
-//		{
-//
-//			if (visited[i] == false && cf[s][i] != maxC)
-//			{
-//				visited[i] = true;
-//				q.push(i);
-//				path[i] = s;
-//				if (i == B) // have path from A to B
-//				{
-//					isFound = true;
-//					return isFound;
-//				}
-//			}
-//		}
-//	}
-//	return isFound;
-//}
-////increase flow following the increasing flow path (have just found by BFS)
-//void IncFlow()
-//{
-//	int u, v, IncValue;
-//	// follow the path to find the smallest weight of bows on path
-//	IncValue = maxC;
-//	v = B;
-//	while (v != A)
-//	{
-//		u = path[v];
-//		if (abs(cf[u][v]) < IncValue)
-//		{
-//			IncValue = abs(cf[u][v]);
-//		}
-//		v = u;
-//	}
-//	// follow the path the second time to increase flow
-//	v = B;
-//	while (v != A)
-//	{
-//		u = path[v];
-//		if (cf[u][v] > 0)
-//		{
-//			f[u][v] = f[u][v] + IncValue; // (u,v ) is a normal bow
-//		}
-//		else
-//		{
-//			f[u][v] = f[u][v] - IncValue; // (u,v) is a inverse bow
-//		}
-//		v = u;
-//	}
-//}
-////print the max-flow
-//void Print()
-//{
-//	int m = 0;
-//	for (int u = 0; u < n; u++)
-//	{
-//		for (int v = 0; v < n; v++)
-//		{
-//			if (c[u][v] > 0)
-//			{
-//				cout << "f[" << u << "][" << v << "] = " << f[u][v] << endl;
-//				if (u == A)
-//				{
-//					m = m + f[A][v];
-//				}
-//			}
-//		}
-//	}
-//	cout << "Max flow: " << m << endl;
-//}
-//int main()
-//{
-//	Enter();
-//	for (int i = 0; i < MAX; i++)
-//	{
-//		for (int j = 0; j < MAX; j++)
-//		{
-//			f[i][j] = 0;
-//		}
-//	}
-//
-//	do
-//	{
-//		CreateGf();
-//		if (FindPath() == false)
-//			break;
-//		IncFlow();
-//	} while (true);
-//	Print();
-//	return 0;
-//}
+// //#include <iostream>
+// //#include <queue>
+// //using namespace std;
+// //
+// //int const max = 100;
+// //int const maxc = 1000;
+// //int c[max][max], f[max][max], cf[max][max];
+// //int path[max]; // trace path
+// //			   // n is the number of vertices
+// //			   //a is source vertice
+// //			   //b is sink vertice
+// //int n, a, b;
+// //
+// ////input the arguments
+// //void enter()
+// //{
+// //	// m is the number of edges
+// //	int m, u, v;
+// //	for (int i = 0; i < max; i++)
+// //	{
+// //		for (int j = 0; j < max; j++)
+// //		{
+// //			c[i][j] = 0;
+// //		}
+// //	}
+// //	cin >> n >> m >> a >> b;
+// //	a--; // the first vertice begins at 0
+// //	b--; // the sink vertice ends at n - 1
+// //	for (int i = 0; i< m; i++)
+// //	{
+// //		cin >> u >> v; // two adjacent vertices
+// //		cin >> c[u - 1][v - 1]; // weight
+// //	}
+// //}
+// //// find increasing flow
+// //void creategf()
+// //{
+// //	//build cf from c and f
+// //	for (int u = 0; u < n; u++)
+// //	{
+// //		for (int v = 0; v <n; v++)
+// //		{
+// //			cf[u][v] = maxc;
+// //		}
+// //	}
+// //	for (int u = 0; u < n; u++)
+// //	{
+// //		for (int v = 0; v <n; v++)
+// //		{
+// //			if (c[u][v] > 0) // if (u,v) is a bow in g
+// //			{
+// //				if (f[u][v] < c[u][v])
+// //				{
+// //					cf[u][v] = c[u][v] - f[u][v]; // normal bow
+// //				}
+// //				if (f[u][v] > 0)// inverse bow
+// //				{
+// //					cf[v][u] = -f[u][v];
+// //				}
+// //			}
+// //		}
+// //	}
+// //}
+// ////find a path from a to b using bfs
+// //bool findpath()
+// //{
+// //	queue<int> q;
+// //	bool isfound = false;
+// //	bool visited[max] = { 0 };
+// //	q.push(a);
+// //	visited[a] = true;
+// //	while (!q.empty())
+// //	{
+// //		int s = q.front();
+// //		q.pop();
+// //		for (int i = 0; i < n; i++)
+// //		{
+// //
+// //			if (visited[i] == false && cf[s][i] != maxc)
+// //			{
+// //				visited[i] = true;
+// //				q.push(i);
+// //				path[i] = s;
+// //				if (i == b) // have path from a to b
+// //				{
+// //					isfound = true;
+// //					return isfound;
+// //				}
+// //			}
+// //		}
+// //	}
+// //	return isfound;
+// //}
+// ////increase flow following the increasing flow path (have just found by bfs)
+// //void incflow()
+// //{
+// //	int u, v, incvalue;
+// //	// follow the path to find the smallest weight of bows on path
+// //	incvalue = maxc;
+// //	v = b;
+// //	while (v != a)
+// //	{
+// //		u = path[v];
+// //		if (abs(cf[u][v]) < incvalue)
+// //		{
+// //			incvalue = abs(cf[u][v]);
+// //		}
+// //		v = u;
+// //	}
+// //	// follow the path the second time to increase flow
+// //	v = b;
+// //	while (v != a)
+// //	{
+// //		u = path[v];
+// //		if (cf[u][v] > 0)
+// //		{
+// //			f[u][v] = f[u][v] + incvalue; // (u,v ) is a normal bow
+// //		}
+// //		else
+// //		{
+// //			f[u][v] = f[u][v] - incvalue; // (u,v) is a inverse bow
+// //		}
+// //		v = u;
+// //	}
+// //}
+// ////print the max-flow
+// //void print()
+// //{
+// //	int m = 0;
+// //	for (int u = 0; u < n; u++)
+// //	{
+// //		for (int v = 0; v < n; v++)
+// //		{
+// //			if (c[u][v] > 0)
+// //			{
+// //				cout << "f[" << u << "][" << v << "] = " << f[u][v] << endl;
+// //				if (u == a)
+// //				{
+// //					m = m + f[a][v];
+// //				}
+// //			}
+// //		}
+// //	}
+// //	cout << "max flow: " << m << endl;
+// //}
+int main()
+{
+	enter();
+	for (int i = 0; i < max; i++)
+	{
+		for (int j = 0; j < max; j++)
+		{
+			f[i][j] = 0;
+		}
+	}
+
+	do
+	{
+		creategf();
+		if (findpath() == false)
+			break;
+		incflow();
+	} while (true);
+	print();
+	return 0;
+}
