aa381b41069647cb1b77dba75a3a2ee9078bd788
Tin Nguyen <tinnguyen@Tins-MBP.lan>
2022-08-29 21:15:35 -0500

    mark a with k, b+1 with -k

---
+++ hackerrank/datastructure/array/array_manipulation.py
@@ -0,0 +1,15 @@
+def arrayManipulation(n, queries):
+    # Write your code here
+    arr = [0 for i in range(n)]
+    for i in queries:
+        arr[i[0] - 1] += i[2]
+        if i[1] != len(arr):
+            arr[i[1]] -= i[2]
+    maxval = 0
+    itt = 0
+    print(arr)
+    for q in arr:
+        itt += q
+        if itt > maxval:
+            maxval = itt
+    return maxval
